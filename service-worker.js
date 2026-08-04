8446c0aa22932399dff31abea99e8be5d19a9795
thanwyaa <fbwk4974@gmail.com>
2026-08-04 14:01:55 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,3 @@
-const CACHE_NAME = 'yalla-chemistry-v1';
-const urlsToCache = [
-  '/',
-  '/manifest.json',
-  'https://res.cloudinary.com/dbahe7lxz/image/upload/v1785593596/idraaak/x0xgxrk0kkxxgn73npal.png',
-  'https://res.cloudinary.com/dbahe7lxz/image/upload/v1785593692/idraaak/ogvolfsxxnvk24gho8eb.jpg'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      );
-    })
-  );
-});
+self.addEventListener('install', e => e.waitUntil(caches.open('yalla-v1').then(c => c.addAll(['/'])).then(() => self.skipWaiting())));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(k => Promise.all(k.filter(k => k !== 'yalla-v1').map(k => caches.delete(k))))));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
